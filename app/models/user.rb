4b9e4e46b4f55e1bbfdba02821659b868a167f16
Emanuel Jose <emanuel.jsa21@gmail.com>
2022-01-05 11:11:38 -0300

    Add user association and info in articles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  validates :username, presence :true, uniqueness: { case_sensitive: false }, length: { minimum:3, maximum:25 }
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum:3, maximum:25 }
   VALID_EMAIL = /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
-  validates :email, presence :true, uniqueness: { case_sensitive: false }  length: { maximum:105 }, format: { with: VALID_EMAIL }
-
+  validates :email, presence: true, uniqueness: { case_sensitive: false },  length: { maximum:105 }, format: { with: VALID_EMAIL }
+  
+  has_many :articles
 end
