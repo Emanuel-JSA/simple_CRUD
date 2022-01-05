eb4ea169d03667ade806529e81e44244fd784ff6
Emanuel Jose <emanuel.jsa21@gmail.com>
2022-01-05 11:15:00 -0300

    Add before save action to model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase}
+
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum:3, maximum:25 }
   VALID_EMAIL = /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
   validates :email, presence: true, uniqueness: { case_sensitive: false },  length: { maximum:105 }, format: { with: VALID_EMAIL }
