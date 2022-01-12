2f8fc87a4a5152356957dbe33c4885a497cab2e7
Emanuel Jose <emanuel.jsa21@gmail.com>
2022-01-12 10:52:46 -0300

    Add categories and article many-to-many association

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,6 @@ class Article < ApplicationRecord
   validates :description, presence: true
   
   belongs_to :user
+  has_many :article_categories
+  has_many :categories, through: :article_categories
 end
